42a008ece910cee40ea706417312485b584a856e
Tom Denley <tom.denley@youdevise.com>
2011-05-09 09:28:54 +0100

    get the stack working a little better

--- src/main/java/org/netmelody/menodora/JasmineSpecFileWrapper.java
+++ src/main/java/org/netmelody/menodora/JasmineSpecFileWrapper.java
@@ -3,10 +3,14 @@ package org.netmelody.menodora;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Stack;
 
 import org.junit.runner.Description;
 import org.mozilla.javascript.CompilerEnvirons;
 import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.FunctionNode;
 import org.mozilla.javascript.Node;
 import org.mozilla.javascript.Parser;
 import org.mozilla.javascript.ScriptOrFnNode;
@@ -60,19 +64,20 @@ public class JasmineSpecFileWrapper {
         }
 
         public void parseFunction(ScriptOrFnNode node) {
-            Description desc = description;
-            parse(node);
+            Stack<ScriptOrFnNode> functions = new Stack<ScriptOrFnNode>();
             int functionCount = node.getFunctionCount();
-            for (int i = 0; i < functionCount; i++) {
-                parseFunction(node.getFunctionNode(i));
-                description = desc;
+            for (int i = functionCount - 1; i >= 0; i--) {
+                FunctionNode functionNode = node.getFunctionNode(i);
+                functions.push(functionNode);
             }
+            parse(node, functions);
         }
         
-        private void parse(Node node) {
+        private void parse(Node node, Stack<ScriptOrFnNode> functions) {
             if (node == null) {
                 return;
             }
+            Description parentDesc = description;
             if (node.getType() == Token.NAME ) {
                 if ("describe".equals(node.getString())) {
                     final Description suiteDesc = Description.createSuiteDescription(node.getNext().getString());
@@ -84,12 +89,17 @@ public class JasmineSpecFileWrapper {
                 }
                 System.out.println(node.getString());
             }
-            if (node.getType() == Token.STRING ) {
+            if (node.getType() == Token.STRING) {
                 System.out.println(node.getString());
             }
+            if (node.getType() == Token.FUNCTION && !(node instanceof ScriptOrFnNode)) {
+                parseFunction(functions.pop());
+            }
             
-            parse(node.getFirstChild());
-            parse(node.getNext());
+            parse(node.getFirstChild(), functions);
+            description = parentDesc;
+            
+            parse(node.getNext(), functions);
         }
     }
 }
