01e668a2e375da8b7458d65d4d5139648c68fff1
Tom Denley <tom.denley@youdevise.com>
2011-05-13 11:23:48 +0100

    added separate "with-dom" suite

---
+++ src/test/resources/dummyproj/src/test/java/org/netmelody/dummy/JsTestsWithDom.java
@@ -0,0 +1,15 @@
+package org.netmelody.dummy;
+
+import org.junit.runner.RunWith;
+import org.netmelody.menodora.JasmineJavascriptContext;
+import org.netmelody.menodora.JasmineSuite;
+
+@RunWith(JasmineSuite.class)
+@JasmineJavascriptContext(
+        source="dummyjs/*.js",
+        jasmineHelpers="dummyjstests/*.js",
+        jasmineSpecs="dummyjstests/*Spec.js",
+        withSimulatedDom=true)
+public class JsTestsWithDom {
+
+}
