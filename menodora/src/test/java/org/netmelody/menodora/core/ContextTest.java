40ff901ce14c72d6dafc266e96324174cfba6f4e
Samir Talwar <samir@noodlesandwich.com>
2016-02-27 23:09:40 +0000

    Separate the Context test from Jasmine; use a fake "style".

--- menodora/src/test/java/org/netmelody/menodora/core/ContextTest.java
+++ menodora/src/test/java/org/netmelody/menodora/core/ContextTest.java
@@ -8,22 +8,27 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
 
 public final class ContextTest {
+
+    private static final String JAVASCRIPT_JAR_FILE = "dummy-project.jar";
+    private static final String TEST_CLASS_NAME = "org.netmelody.menodora.dummy.JsTests";
+
     @Test public void
     canScanInAJar() throws Exception {
-        final Class<?> suiteClass = Class.forName("org.netmelody.dummy2.JsTests", true, createClassLoader());
+        final Class<?> suiteClass = Class.forName(TEST_CLASS_NAME, true, createClassLoader());
         final Context context = new Context(suiteClass);
 
         final Iterable<String> locate = context.reflectionsExample();
         assertThat(locate, hasItems(
-                "dummyjs/Song.js",
-                "dummyjs/Player.js",
-                "dummyjstests/SpecHelper.js",
-                "dummyjstests/PlayerSpec.js"));
+                "org/netmelody/menodora/dummy/main/One.js",
+                "org/netmelody/menodora/dummy/main/Two.js",
+                "org/netmelody/menodora/dummy/test/OneTest.js",
+                "org/netmelody/menodora/dummy/test/TwoTest.js",
+                "org/netmelody/menodora/dummy/test/TestHelper.js"));
     }
 
     private static ClassLoader createClassLoader() {
         return new URLClassLoader(new URL[] {
-                ContextTest.class.getResource("/dummyproj2.jar")
+                ContextTest.class.getClassLoader().getResource(JAVASCRIPT_JAR_FILE)
         });
     }
 }
